6e9226f17868020faede803d2b649f3a6e19710a
EHronek <emmanuelhronek@gmail.com>
2025-02-15 18:02:14 +0300

    status api

---
+++ api/v1/app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Flask app"""
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+import os
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the current db session"""
+
+    storage.close()
+
+@app.route('/test', methods=['GET'])
+def test():
+    return "Flask is working"
+
+
+if __name__ == "__main__":
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0' )
+    port = int(os.getenv('HBNB_API_PORT', 5000))
+
+    app.run(host=host, port=port, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""index.py"""
+from flask import jsonify
+from api.v1.views import app_views
+
+print("index file loaded well")
+
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
+def status():
+    """Returns the status of the api"""
+    return jsonify({"status":"OK"})
